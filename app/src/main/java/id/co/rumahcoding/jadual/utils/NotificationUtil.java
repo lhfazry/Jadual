3390a2f855ae6ab28b3cb6d612a8a048e3ff2e99
Lhuqita Fazry <lhuqita.fazry@gmail.com>
2017-06-01 03:17:01 +0700

    Fix notification bug

--- app/src/main/java/id/co/rumahcoding/jadual/utils/NotificationUtil.java
+++ app/src/main/java/id/co/rumahcoding/jadual/utils/NotificationUtil.java
@@ -24,7 +24,7 @@ public class NotificationUtil {
         Builder builder = new Builder(context)
         .setContentTitle(title)
         .setContentText(subject)
-        //.setSmallIcon(R.drawable.ic_small_icon)
+        .setSmallIcon(R.mipmap.ic_launcher)
                 .setLargeIcon(bitmap)
         .setContentIntent(contentIntent);
 
